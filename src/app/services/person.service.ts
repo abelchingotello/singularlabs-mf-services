21cfbaec56327e5faba7a2f2850412b122109ca3
paulVera2001 <pveramolla@gmail.com>
2025-10-06 16:11:11 -0500

    actualizacion de url a apigatway en mf-services

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -8,8 +8,8 @@ import { environment } from 'src/environments/environment';
 })
 export class PersonService {
 
-  //private url = `${environment.URL_API_GATEWAY}`;
-  private url = `${environment.URL_API_LOCAL}`; //LAMBDA LOCAL
+  private url = `${environment.URL_API_GATEWAY}`;
+  //private url = `${environment.URL_API_LOCAL}`; //LAMBDA LOCAL
 
   constructor(
     private httpClient: HttpClient,
