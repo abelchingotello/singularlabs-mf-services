425485686b08139707cbe3c116863c8d19ce6d18
paulVera2001 <pveramolla@gmail.com>
2026-01-01 22:49:29 -0500

    mejora en mf-services para mostrar solo personas habilitadas o todas

--- src/app/components/pages/assign/assign/assign.component.ts
+++ src/app/components/pages/assign/assign/assign.component.ts
@@ -100,7 +100,7 @@ export class AssignComponent implements OnInit {
   listData() {
     this.spinner.spinnerOnOff();
     forkJoin([
-      this.personService.getPerson('RECAUDADORA DE SERVICIOS'),
+      this.personService.getPerson('RECAUDADORA DE SERVICIOS',undefined,true),
       this.masterService.getItemsMasterTable('14'), // CategoriaService
       this.masterService.getItemsMasterTable('1') // EStados
     ]).subscribe({
