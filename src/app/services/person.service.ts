80205d2d4312d12d5fc11abcc9af31918749805c
paulVera2001 <pveramolla@gmail.com>
2026-01-21 18:25:31 -0500

    limpieza de codigo en mf-services sobre funciones de usuario no usada

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -18,17 +18,13 @@ export class PersonService {
     return this.httpClient.post(`${this.url}/person`, data);
   }
 
-  postIdPerson(id:string):Observable<any> {
-    return this.httpClient.post(`${this.url}/person/${id}`,null);
-  }
-
 
   getPerson(typeEntity?:string,nameAlias?:string, activeOnly?: boolean):Observable<any> {
     let params = new HttpParams();
     if(typeEntity) params = params.set('typeEntity', typeEntity);
     
     if(nameAlias) params = params.set('nameAlias', nameAlias);
-    
+
     if (activeOnly) params = params.set('activeOnly', JSON.stringify(activeOnly));
 
     return this.httpClient.get(`${this.url}/person/entity`, {params:params});
