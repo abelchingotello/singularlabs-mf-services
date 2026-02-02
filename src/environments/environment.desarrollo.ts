4957a82d5070b8ac6c9411c4ddd5fb79baf5d7aa
DESKTOP-5OM5M2B\Saul <leonsaul667@gmail.com>
2026-02-02 18:32:36 -0500

    updates: Environment desas

--- src/environments/environment.desarrollo.ts
+++ src/environments/environment.desarrollo.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   URL_API_GATEWAY:'https://rrmnpgfzii.execute-api.us-east-1.amazonaws.com/sandbox',
-  URL_API_GENERATE_QR: 'https://wgt4e9f8c9.execute-api.us-east-1.amazonaws.com/DEVjosue',
+  URL_API_GENERATE_QR: 'https://n48ne1w1vb.execute-api.us-east-1.amazonaws.com/dev',
   URL_API_GENERATE_QR_API_KEY: 'BSTFcTujZl7M1HBM5HwMuqm6xZUFkTQ3W1hjhjK3',
 };
