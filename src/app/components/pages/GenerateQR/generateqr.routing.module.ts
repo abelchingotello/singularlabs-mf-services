35c632ab3144335670d7e9ce45aa57863a553167
DESKTOP-5OM5M2B\Saul <leonsaul667@gmail.com>
2026-02-04 18:32:38 -0500

    Updates: Agregar Reprocesamientos Pagos

--- src/app/components/pages/GenerateQR/generateqr.routing.module.ts
+++ src/app/components/pages/GenerateQR/generateqr.routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GenerateQR } from './generateqr.component';
 import { GenerateQrReportsComponent } from './reports/generateqr-reports.component';
+import { GenerateQrReprocesamientoPagosComponent } from './reprocesamiento-pagos/generateqr-reprocesamiento-pagos.component';
 
 const routes: Routes = [
   { path: '', component: GenerateQR },
   { path: 'generate', component: GenerateQR },
   { path: 'reports', component: GenerateQrReportsComponent },
+  { path: 'reprocesamiento-pagos', component: GenerateQrReprocesamientoPagosComponent },
 
  // { path: 'import', component: ImportServicesComponent },
 
