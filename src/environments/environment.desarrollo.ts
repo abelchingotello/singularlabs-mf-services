97703b1970fbbf3aaf1d35c379fb5aeb34a0c42d
DESKTOP-5OM5M2B\Saul <leonsaul667@gmail.com>
2026-02-18 17:38:59 -0500

    updates

--- src/environments/environment.desarrollo.ts
+++ src/environments/environment.desarrollo.ts
@@ -3,4 +3,6 @@ export const environment = {
   URL_API_GATEWAY:'https://rrmnpgfzii.execute-api.us-east-1.amazonaws.com/sandbox',
   URL_API_GENERATE_QR: 'https://n48ne1w1vb.execute-api.us-east-1.amazonaws.com/dev',
   URL_API_GENERATE_QR_API_KEY: '',
+  URL_API_GENERATE_QR_USERNAME: 'qrligopay',
+  URL_API_GENERATE_QR_PASSWORD: 'QrLigo@2026!Tmp9',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,4 +12,6 @@ export const environment = {
 
   URL_API_GENERATE_QR: 'https://n48ne1w1vb.execute-api.us-east-1.amazonaws.com/dev',
   URL_API_GENERATE_QR_API_KEY: '',
+  URL_API_GENERATE_QR_USERNAME: 'qrligopay',
+  URL_API_GENERATE_QR_PASSWORD: 'QrLigo@2026!Tmp9',
 };
